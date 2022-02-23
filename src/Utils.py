9726680ba36e5a178436f4c4a1bbb868eeb337e4
Levi Butcher <levibutcher355@gmail.com>
2022-02-22 20:54:18 -0500

    Merging of NHANSE and mortality data

--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import functools
+import numpy as np
 
 mortality_colspecs = [(0, 13),
                       (14, 14),
@@ -16,33 +17,50 @@ mortality_colspecs = [(0, 13),
 
 mortality_widths = [14, 1, 1, 3, 1, 1, 1, 4, 3, 3, 5, 6]
 
-mortality_colnames = ["publicid",
-                      "eligstat",
-                      "mortstat",
-                      "ucod_leading",
-                      "diabetes",
-                      "hyperten",
-                      "dodqtr",
-                      "dodyear",
-                      "permth_int",
-                      "permth_exm",
-                      "wgt_new",
-                      "sa_wgt_new"]
 
-drop_columns = ["publicid",
-                "dodqtr",
-                "dodyear",
-                "wgt_new",
-                "sa_wgt_new"]
+def toUpperCase(l):
+    return list(map(lambda l: l.upper(), l))
 
 
-def read_mortstat(path):
-    data = pd.read_fwf(path, widths=mortality_widths)
+mortality_colnames = toUpperCase(["publicid",
+                                  "eligstat",
+                                  "mortstat",
+                                  "ucod_leading",
+                                  "diabetes",
+                                  "hyperten",
+                                  "dodqtr",
+                                  "dodyear",
+                                  "permth_int",
+                                  "permth_exm",
+                                  "wgt_new",
+                                  "sa_wgt_new"])
+
+drop_columns = toUpperCase(["publicid",
+                            "dodqtr",
+                            "dodyear",
+                            "wgt_new",
+                            "sa_wgt_new"])
+
+
+def read_mortstat(location):
+    # location = PATH | URL
+    data = pd.read_fwf(location, widths=mortality_widths)
     data.columns = mortality_colnames
-    data = data.assign(seqn=data.publicid.apply(lambda x: str(x)[0:4]))
+    data = data.assign(SEQN=data.PUBLICID.apply(lambda x: str(x)[:8]))
     data = functools.reduce(lambda acc, x: acc.drop(
         x, axis=1), drop_columns, data)
-    return data
+
+    # Convert data to correct types
+    # https://www.cdc.gov/nchs/data/datalinkage/public-use-2015-linked-mortality-files-data-dictionary.pdf
+    data.SEQN = pd.to_numeric(data.SEQN, errors="coerce", downcast="float")
+    data.MORTSTAT = pd.to_numeric(
+        data.MORTSTAT, errors="coerce")
+    data.DIABETES = pd.to_numeric(
+        data.DIABETES, errors="coerce")
+    data.HYPERTEN = pd.to_numeric(
+        data.HYPERTEN, errors="coerce")
+
+    return data.set_index("SEQN")
 
 
 def build_morstat_download_url(year):
@@ -52,3 +70,38 @@ def build_morstat_download_url(year):
 
 def compose(f, g):
     return lambda x: f(g(x))
+
+
+def get_mortstat_data(years):
+    data = map(compose(read_mortstat,
+               build_morstat_download_url), years)
+    return pd.concat(data)
+
+
+def get_nhanse_data(year_data_files):
+    data = []
+    for year, data_files in year_data_files:
+        data.append([read_nhanse_data(build_nhanse_url(year, f))
+                    for f in data_files])
+
+    data = [functools.reduce(lambda acc, x: acc.merge(
+        x, on="SEQN", how="inner"), l) for l in data]
+
+    return pd.concat(data)
+
+
+def build_nhanse_url(year, data_file):
+    s, e = year
+    return f"https://wwwn.cdc.gov/Nchs/Nhanes/{s}-{e}/{data_file}"
+
+
+def read_nhanse_data(location):
+    # location = PATH | URL
+    return pd.read_sas(location).set_index('SEQN')
+
+
+def get_nhanse_mortality_dataset(year_data_files):
+    nhanse_years = [y for y, _ in year_data_files]
+    nhanse_data = get_nhanse_data(year_data_files)
+    mortality_data = get_mortstat_data(nhanse_years)
+    return nhanse_data.join(mortality_data, on="SEQN", how="inner")
--- src/main.py
+++ src/main.py
@@ -1,24 +1,58 @@
 import pandas as pd
 import Utils
+from sklearn import linear_model
 
 # TODO
-# Need to convert mortstat data to correct data types INT
+# [x] Need to convert mortstat data to correct data types INT
+# [x] Figure out how to Combining NHANSE data
+# [x] Check what the 6 lab measurements were and find there nhanse files
+# [x] Enter all NHANSE Years with correct data files
+# [] Validate if Dataset is correctly put together
+# [x] Setup X,Y from dataset
+# [] Setup 60,30,10 split for training,val, and testing
+# [] Setup graphs of training results
 
-NHANSE_YEARS = [(2001, 2002), (2003, 2004), (2005, 2006)]
-
-
-def get_mortstat_data(years):
-    data = map(Utils.compose(Utils.read_mortstat,
-               Utils.build_morstat_download_url), years)
-    return pd.concat(data)
-
-
-def get_NHANSE_data(years):
-    return 5
+# Forget about LDLC for now
+markers = ["TotalChol", "LDL", "HDL", "FBG", "TG", "LDLC"]
 
 
-data = get_mortstat_data(NHANSE_YEARS)
+NHANSE_DATA_FILES = [
+    ((2005, 2006), ["TCHOL_D.XPT", "TRIGLY_D.XPT", "HDL_D.xpt", "GLU_D.xpt"]),
+    ((2007, 2008), ["TCHOL_E.XPT", "TRIGLY_E.XPT", "HDL_E.xpt", "GLU_E.xpt"])]
 
-print(data.head)
-print(data.seqn.describe())
-print(data.ucod_leading.count())
+# IS Fasting glucose different then glucose
+# All measurements in (mg/dl)
+features_descritions = [("LBXTC", "Total Chol"), ("LBDLDL", "LDL Chol"),
+                        ("LBDHDD", "HDL Chol"), ("LBXGLU", "Fasting Glucose"), ("LBXTR", "Triglyceride")]
+features = [x for x, _ in features_descritions]
+
+dataset = Utils.get_nhanse_mortality_dataset(NHANSE_DATA_FILES)
+
+
+def labelCauseOfDeathAsCVR(ucod_leading):
+    # Different meanings of ucod_leading - https://www.cdc.gov/nchs/data/datalinkage/public-use-2015-linked-mortality-files-data-dictionary.pdf
+    # 1 is Diesease of heart
+    # 2 is Cerebrovascular Diseases
+
+    match ucod_leading:
+        case 0 | 1:
+            return 1
+        case _:
+            return 0
+
+    # print(dataset.columns)
+X = dataset.loc[:, features].assign(
+    CVR=dataset.UCOD_LEADING.apply(labelCauseOfDeathAsCVR)).dropna()
+Y = X.CVR
+X = X.loc[:, features]
+
+# ML Steps
+# Split dataset into train,val, and test
+# Run Training on train set, show performance curve using val, KFold validation
+# Run On test set showing F1 score, precision, and recall.
+
+reg = linear_model.LinearRegression()
+reg.fit(X, Y)
+
+predictY = reg.predict(X)
+print((predictY == Y).count())
